26b2ad932b2b489c1ea2f7849046805c4163b8f1
Adam Chader <adamchader0@gmail.com>
2022-01-25 13:53:11 +0100

    jsp

---
+++ run_poll_workload.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+max_num_threads=5
+ 
+rm poll_data
+
+for i in $(seq 1 $max_num_threads) ; do
+  cd ignite
+  bin/ignite.sh ignite.xml > ../ignite_log 2>&1 & 
+  sleep 5
+  #bin/control.sh --set-state ACTIVE
+  #sleep 10
+  cd ..
+  echo "avant load"
+  load=$(./data_load.sh $i)
+  echo "après load"
+  work=$(./workload_test.sh$i)
+
+  kill -s SIGHUP $(ps -ef | grep -E "java.*ignite|ignite.*java" | head -n 1 | cut -d " " -f "8" )
+
+  nb_exec=$(grep "Nombre d'executions de poll" ignite_log | cut -d ":" -f 2)
+  temps_total=$(grep "Temps total dans poll" ignite_log | cut -d ":" -f 2)
+		
+  printf "%s,%s,%s,%s\n" $i $nb_exec $temps_total $work >> poll_data
+  
+  pkill -f 'ignite' 
+done
+
